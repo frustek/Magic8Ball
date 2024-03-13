03764e7e054326b38635216c5c4b3369043e5b3e
Adam Mroz <mrozadam1992@gmail.com>
2024-03-13 23:06:35 +0100

    Initial Commit

---
+++ Magic8Ball/Magic8BallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic8BallApp.swift
+//  Magic8Ball
+//
+//  Created by Adam Mroz on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct Magic8BallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
